34b3e72f0d2e343342dba5a7e75578ef721d057f
victor <victor14souza@gmail.com>
2021-09-10 09:09:45 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+            <div className="container">
+                <h1>Página home</h1>
+             </div>
+         </div>
+    );
+  }
+  
+  export default Home;
+  
